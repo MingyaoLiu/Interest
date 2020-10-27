51f61c85d1b89770f38cd0f8cc9b4b80f8d937ed
Mingyao Liu <MingyaoLiu@users.noreply.github.com>
2020-10-27 14:27:07 -0400

    add init

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,18 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  fs_c_id: 'VIL0PPC5YZTJUU0TWN5WJEBWNAKISARNPKDEIBOAFBIRODWA',
+  fs_c_scrt: 'UBI3KWU0NVJK0MJJF5LPLIFUZJ5ZZBJQLSAV1GTZGPITFZZN'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
