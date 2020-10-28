d78ec8b27e324e82d4eb22998c2b645a34daecb1
Mingyao Liu <MingyaoLiu@users.noreply.github.com>
2020-10-28 16:27:52 -0400

    add list-panel and test ui

--- frontend/src/app/app.component/app.component.ts
+++ frontend/src/app/app.component/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { GoogleMap } from '@angular/google-maps';
+import { FoursquareService } from '../services/foursquare.service';
+import { WindowRef } from '../services/win-ref.service'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,106 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Interest Map';
+  zoom = 15
+  center: google.maps.LatLngLiteral = { // Default to Ming's Home location
+    lat: 43.4700288,
+    lng: -80.57978880000002
+  }
+  markers = [];
+  fsService: FoursquareService;
+
+  @ViewChild("gmapView") gmap: GoogleMap;
+
+
+  options: google.maps.MapOptions = {
+    zoomControl: false,
+    scrollwheel: true,
+    disableDoubleClickZoom: true,
+    maxZoom: 17,
+    minZoom: 13,
+    disableDefaultUI: true,
+  }
+  constructor(_fsService: FoursquareService) {
+    this.fsService = _fsService
+  }
+
+  ngOnInit() {
+    navigator.geolocation.getCurrentPosition((position)=>{
+      const longitude = position.coords.longitude;
+      const latitude = position.coords.latitude;
+
+      this.center = {
+        lat: latitude,
+        lng: longitude,
+      }
+    });
+  };
+
+  ngAfterViewInit(){
+    this.getNewData()
+    console.log(this.gmap)
+  }
+
+  lockStartDate = new Date(0);
+
+  private async getNewData() {
+    try {
+
+      if (new Date().getTime() - this.lockStartDate.getTime() < 2000) { // To limit the amount of API call to foursquare on free tier plan.
+        return setTimeout(() => { 
+          this.getNewData()
+        }, 1000)
+      }
+
+      this.lockStartDate = new Date()
+      const currentCenter = {
+        lat: this.gmap.getCenter().lat(),
+        lng: this.gmap.getCenter().lng(),
+      }
+      const res = await this.fsService.getInterestXHR(currentCenter);
+
+
+      const venues = res["response"].groups[0].items
+      const venueListLength = venues.length;
+      this.markers = []
+      this.markers = venues.map((elem, index) => {
+            const newMarker = {
+              position: {
+                lat: elem.venue.location.lat,
+                lng: elem.venue.location.lng,
+              },
+              label: {
+                color: 'red',
+                text: ' '
+              },
+              title: elem.venue.name,
+              options: { animation: google.maps.Animation.DROP },
+              zIndex: venueListLength - index 
+            }
+            return newMarker;
+      });
+      this.lockStartDate = new Date(0);
+    } catch (err) {
+      console.log(err);
+      this.lockStartDate = new Date(0);
+    }
+
+      
+  }
+
+  mouseDragStartEvt(evt) {
+    console.log("Drag start, cancel all existing data resolve.")
+    console.log(evt)
+  }
+
+  mouseDragEndEvt($event) {
+    console.log("drag end, find interest based on current center point location.")
+    this.getNewData()
+  }
+
+  mapCenterChangedEvt(evt) { // not used, as it doesn't help with deacceleration end.
+    console.log("CENTER CHANGE")
+    console.log(evt)
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,48 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component/app.component';
+import { environment} from '../environments/environment'
+import { WindowRef } from './services/win-ref.service';
+
+import { HttpClientModule } from '@angular/common/http';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { ListPanelComponent } from './list-panel/list-panel.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListPanelComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GoogleMapsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  
+  providers: [WindowRef],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+//   constructor(protected _winRef: WindowRef) {
+//     this.appendGoogleMapsJSLib()
+//   }
+
+//   private appendGoogleMapsJSLib() {
+//     try {
+//         const gmapScript = document.createElement('script');
+//         gmapScript.src = 'https://maps.googleapis.com/maps/api/js?key='+ environment.g_map +'&callback=initMap';
+//         const win = new WindowRef();
+//         win.nativeWindow.initMap = function() {
+//           console.log("GOOGLE MAP LOADED")
+//           // JS API is loaded and available
+//         };
+//         document.head.appendChild(gmapScript);
+//     } catch (err) {
+//         console.error('Error appending and loading google maps');
+//         console.error(err);
+//     }
+// }
+
+}
